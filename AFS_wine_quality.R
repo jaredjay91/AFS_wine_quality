f9d18f1b00dfc5f77b8bbfc609c38bde87b3063c
Jared <jaredjay91@gmail.com>
2022-02-22 21:37:39 -0700

    Initial commit

---
+++ AFS_wine_quality.R
@@ -0,0 +1,65 @@
+# This script fulfills the assignment outlined in https://www.gastrograph.com/blogs/interviewing-data-science-interns.
+
+# Run it with 'Rscript AFS_wine_quality.R'
+
+# Using only partial least squares regression for generalized linear models (the plsRglm package in R), build an ensemble model to predict the quality score given to each wine from the Vinho Verde region of Portugal (see the data bullet in the Requirements section below to download the datasets). The data consists of chemical tests on wines and an overall assessment score averaged from at least three professional wine tasters. This is interesting data for AFS as the lack of consistent preferences among professional tasters is one of the reasons our company exists.
+
+# The rubric for assessment is explained in the Selection Criteria section below. The prediction model should be trained and will be tested on both red and white wine after joining the two datasets. We will split the supplied data into an 80% training set and a 20% hold-out validation set before running your training script with a random seed. We will use mean absolute error as a performance metric.
+
+#----------------------------------------------------------------
+
+# LOAD LIBRARIES
+#install.packages("mvtnorm")
+#install.packages("bipartite")
+
+# Package 'nloptr' must be installed like this: 
+# Download https://github.com/stevengj/nlopt/archive/v2.7.1.tar.gz
+# Extract contents, then enter the directory and type the following command:
+# cmake . && make && sudo make install
+# next type this command:
+# sudo apt install libnlopt-dev
+# Then you can do this:
+#install.packages("nloptr")
+# 
+
+# Package 'nloptr' is a prerequisite for 'car'
+#install.packages("car")
+
+# Packages 'mvtnorm', 'bipartite', and 'car' are prerequisites for 'plsRglm'.
+#install.packages("plsRglm")
+
+library("plsRglm") # See https://arxiv.org/abs/1810.01005 
+
+#install.packages("tidyverse")
+library("tidyverse") # for tidying up the data set
+
+library("ggplot2") # for visualizations
+
+
+#---------------------------------------------------------------
+
+# LOAD THE DATA
+winequality_red = read.csv("data/winequality-red.csv")
+winequality_white = read.csv("data/winequality-white.csv")
+
+# The goal is to use these input variables:
+#   1 - fixed acidity
+#   2 - volatile acidity
+#   3 - citric acid
+#   4 - residual sugar
+#   5 - chlorides
+#   6 - free sulfur dioxide
+#   7 - total sulfur dioxide
+#   8 - density
+#   9 - pH
+#   10 - sulphates
+#   11 - alcohol
+
+#   To predict this output variable: 
+#   12 - quality (score between 0 and 10)
+
+
+#---------------------------------------------------------------
+
+# EXPLORE THE DATA
+
